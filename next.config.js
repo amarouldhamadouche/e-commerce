b0739d947567998f29b0ec9176712e31148d71f5
amarouldhamadouche <amarouldhamadouche0@gmail.com>
2022-10-28 17:22:26 +0100

    my first commit

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,eslint: {
     ignoreDuringBuilds: true,
 },
-  images: {
+ future: { webpack5: true }, images: {
     domains:['ibb.co','res.cloudinary.com','images.pexels.com','d3o2e4jr3mxnm3.cloudfront.net','cdn.shopify.com','www.prada.com','www.burdastyle.com','images.ctfassets.net','www.vintageindustries.nl','www.pngarts.com','i.ibb.co']
   }
 }
