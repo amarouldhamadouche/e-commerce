2dc8965bb7efaa4a577074f2703960590967db3a
amarouldhamadouche <amarouldhamadouche0@gmail.com>
2022-10-28 16:58:34 +0100

    my first commit

--- next.config.js
+++ next.config.js
@@ -10,3 +10,16 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+
+    if (!isServer) {
+      config.target = 'electron-renderer';
+      config.node = {
+        __dirname: true,
+      }
+    }
+    return config;
+  },
+};
