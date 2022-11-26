d35acf6acbeeed850a17ad0d856cea4ed6edd610
amar <66710686+amarouldhamadouche@users.noreply.github.com>
2022-11-26 17:34:59 +0100

    Update Categories.jsx

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -2,8 +2,22 @@
 import CategorieItem from "./CategorieItem"
 import styles from "../styles/Categories.module.css"
 import axios from 'axios'
+import {useState,useEffect} gtom 'react'
 
-export default function Categories({categories}){
+export default function Categories(){
+const [categories,setCategories] = useState([])
+
+  useEffect(()=>{
+    const fetchCats = async()=>{
+      try{
+       const res = typeof(window)!=="undefined" && await axios.get(`${window.location.origin}/api/categories/`)
+       setCategories(res.data)
+       
+      }catch(err){
+      }
+    }
+    fetchCats()
+  },[])
 
  
 
